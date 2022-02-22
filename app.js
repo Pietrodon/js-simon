360248d1f802b176089e869541326171d7aad323
Pietrodon <donvitopietro00@gmail.com>
2022-02-22 16:01:46 +0100

    layout di base

---
+++ app.js
@@ -0,0 +1,5 @@
+// Visualizzare in pagina 5 numeri casuali. ( con un alert )
+// Dopo la chiusura dell’alert parte un timer di 30 secondi.
+// Alla fine dei 30 secondi l’utente dovrà inserire, uno alla volta, i numeri che ha visto precedentemente ( se li ricorda ), tramite il prompt() (servirà un ciclo…?).
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
